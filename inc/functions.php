246fc277b5465841e49c3209fe9584856925ffa4
B5L7 <21278117+B5L7@users.noreply.github.com>
2019-11-28 23:58:52 +0000

    Comments galore for functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -7,17 +7,34 @@
 
 include 'quotes.php';
 
+// I have created a separate quotes file to keep this one simple
+
 // Create the getRandomQuote function and name it getRandomQuote
 
 function getRandomQuote($var) {
 
+//  This is not required in the exercise, but I will first check that the parameter is an array
+
    if (is_array($var)) {
 
+//  If it is, we can start by selecting a quote via a random key
+
      $key = mt_rand(0, 2);
+
+//     This is the most important step in this exercise
+//     We are going to create a new associative array from the random $quotes elements
+//     This new array $package will have a consistent number of keys
+//     With null values for non-mandatory keys that are unspecified
+//     This will simplify the job of outputting HTML from printQuote()
 
      $package['quote'] = $var[$key]['quote'];
 
      $package['source'] = $var[$key]['source'];
+
+//     The quote and source keys are mandatory in $quotes
+//     We can add them to $package without checking that they are specified
+//     However, citation and year are non-mandatory keys
+//     If either is unspecified, we will describe their value as null
 
         if (isset($var[$key]['citation'])) {
         $package['citation'] = $var[$key]['citation'];
@@ -32,6 +49,9 @@ function getRandomQuote($var) {
         }
 
    }
+
+//  We need the value of the array to be returned,
+//  so that we can assign it to a new variable in printQuote()
 
   return $package;
 
