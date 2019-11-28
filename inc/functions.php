f857feacd607f32e3a79f1269bf72537229dce53
B5L7 <21278117+B5L7@users.noreply.github.com>
2019-11-28 21:56:16 +0000

    Created getRandomQuote function

--- inc/functions.php
+++ inc/functions.php
@@ -7,44 +7,37 @@
 
 include 'quotes.php';
 
-//echo '<pre>';
-//var_dump($quotes);
-//echo '</pre>';
-
 // Create the getRandomQuote function and name it getRandomQuote
 
 function getRandomQuote($var) {
 
    if (is_array($var)) {
-     $key = mt_rand(1, 3);
-     echo $key;
+     $key = mt_rand(0, 2);
+     echo $key . '<br/>';
+
+     $quote = $var[$key]['quote'];
+     var_dump($quote);
+
+     $source = $var[$key]['source'];
+     var_dump($source);
+
+
+        if (isset($var[$key]['citation'])) {
+        $citation = $var[$key]['citation'];
+          var_dump($citation);
+
+      }
+
+        if (isset($var[$key]['year'])){
+        $year = $var[$key]['year'];
+          var_dump($year);
+
+      }
+
    }
 }
 
 getRandomQuote($quotes);
-
-
-
-
-
-        // if ($var[$key]['citation']) {
-     //   echo "This quote DOES have a citation";
-//   } else {
-     //   echo "No citation";
-     // }
-
-
-    // check that the variable is an array
-    // if it is, create a logic branch that returns a quote from the array
-    // to do this, create a random number
-    // for the time being, let's limit the range of random to the number of keys in the quotes array
-    // return the value for the random key
-
-
-    // What should we do with citation and year?
-    // Also, does the index file expect the quote and citation to be returned independently?
-    // If so, what is the best way to hand this data to the printQuote function?
-
 
 
 // Create the printQuote funtion and name it printQuote
