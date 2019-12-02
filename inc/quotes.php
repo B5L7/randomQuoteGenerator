54fe1026b464449abd78a2738a1da8597747a166
B5L7 <21278117+B5L7@users.noreply.github.com>
2019-12-02 13:51:51 +0000

    Added final quotes

--- inc/quotes.php
+++ inc/quotes.php
@@ -3,59 +3,57 @@
 $quotes = [];
 
 $quotes[] = [
-'quote' => 'First quote',
-'source' => 'First source',
-'citation' => 'First person',
+'quote' => "Education is an ornament in prosperity and a refuge in adversity",
+'source' => 'The Dialogues',
+'citation' => 'Aristotle',
 ];
 
 $quotes[] = [
-'quote' => 'Second quote',
-'source' => 'Second source',
+'quote' => "Wise men speak because they have something to say, fools because they have to say something",
+'source' => 'The Internet',
 ];
 
 $quotes[] = [
-'quote' => 'Third words of wisdom',
-'source' => 'Third source',
-'citation' => 'Third person',
+'quote' => "I'd like to make you laugh for about ten minutes, though I'm gonna be on for an hour",
+'source' => 'Pryor Live',
+'citation' => 'Richard Pryor',
 'year' => 1980,
 'tag' => 'Quote type: humorous',
 ];
 
 $quotes[] = [
-'quote' => 'Fourth quote',
-'source' => 'Fourth source',
-'citation' => 'Fourth person',
+'quote' => "The most virtuous are those who content themselves with being virtuous, without seeking to appear so",
+'source' => 'Thoughts on Education',
+'citation' => 'Plato',
 'tag' => 'Quote type: classical',
 ];
 
 $quotes[] = [
-'quote' => 'Fifth words of wisdom',
-'source' => 'Fifth source',
-'citation' => 'Fifth person',
-'year' => 1980,
+'quote' => "If you think in terms of a year, plant a seed; if in terms of ten years, plant trees; if in terms of 100 years, teach the people",
+'source' => 'Hundren Schools of Thought',
+'citation' => 'Confucius',
+'year' => '501 BC',
 'tag' => 'Quote type: hyperbole',
 ];
 
 $quotes[] = [
-'quote' => 'Sixth quote',
-'source' => 'Sixth source',
-'citation' => 'Sixth person',
-'tag' => 'Quote type: humourous',
+'quote' => "If you live to be one hundred, you've got it made. Very few people die past that age",
+'source' => 'The Comedies',
+'citation' => 'George Burns',
+'tag' => 'Quote type: humorous',
 ];
 
 $quotes[] = [
-'quote' => 'Seventh quote',
-'source' => 'Seventh source',
-'citation' => 'Seventh person',
-'tag' => 'Quote type: hyperbole',
+'quote' => "I refuse to join any club that would have me as a member",
+'source' => 'Groucho And Me',
+'citation' => 'Groucho Marx',
+'tag' => 'Quote type: humorous',
 ];
 
 $quotes[] = [
-'quote' => 'Eighth quote',
-'source' => 'Eighth source',
+'quote' => "Orators are most vehement when their cause is weak",
+'source' => "Cicero's defense",
 'tag' => 'Quote type: classical',
 ];
-
-
 
 ?>
