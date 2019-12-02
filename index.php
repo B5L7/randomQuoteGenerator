7a640f623bc9da7b9ee214aa68bb049bcf606f4a
B5L7 <21278117+B5L7@users.noreply.github.com>
2019-12-02 12:55:07 +0000

    added auto-refresh js function
    1. Created refreshMe() in head.
    2. Called function in body tag.

--- index.php
+++ index.php
@@ -6,8 +6,24 @@
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/styles.css">
+  <script type = "text/JavaScript">
+         <!--
+            function refreshMe(t) {
+               setTimeout("location.reload();", t);
+            }
+         //-->
+      </script>
 </head>
-<body <?php include 'inc/setColor.php'; ?> >
+
+  <body <?php include 'inc/setColor.php'; ?> onLoad="refreshMe(20000)">
+
+<!--
+  It was not clear from the assignment notes whether we should use js or php to randomise background colour, so I have used the latter.
+
+  In the same body tag I have called the js function refreshMe(), which will auto-refresh the page after 20 seconds.
+
+-->
+
   <div class="container">
     <div id="quote-box">
       <?php include 'inc/functions.php'; ?>
